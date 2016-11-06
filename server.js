3b00479104a5fd4e87a7fd7cb2b01817961c5500
enone <inan_k@hotmail.com>
2016-11-06 11:46:34 -0800

    heroku port update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 
 var app = express();
+const PORT  = process.env.PORT || 5000;
+
 app.use(express.static('public'));
 
-app.listen(5000, function(){
-  console.log('running at port 5000');
+app.listen(PORT, function(){
+  console.log('running at port ' + PORT);
 });
